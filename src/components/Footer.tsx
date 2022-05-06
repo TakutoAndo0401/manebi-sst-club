f72515a1e7097a5ba202d572ebd3ae80362eab58
TakutoAndo <terminus.rita@gmail.com>
2022-05-06 16:02:38 +0900

    fix: 一旦削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 // フッター
 const Footer: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline ">フッター</h1>;
+  return <h1 className="text-3xl font-bold underline "></h1>;
 };
 
 export default Footer;
